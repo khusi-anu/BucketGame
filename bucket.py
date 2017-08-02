b56e31b7585a675eab220dc92a8ea9a2f67c6250
Anu Kumari Gupta <jyotianugpt@gmail.com>
2017-08-02 21:19:36 +0530

    This is my Python Game

--- bucket.py
+++ bucket.py
@@ -13,8 +13,8 @@ def display(p):
     #s+=" ".join(map(str,L[9]))
     return L
 def bucket(n):
-    print ">>>>Enter 1 to fill the bucket A \nEnter 2 to fill the bucket B \nEnter 3 to fill the bucket C <<<<"
-    tap=input()
+    print(">>>>Enter 1 to fill the bucket A \nEnter 2 to fill the bucket B \nEnter 3 to fill the bucket C <<<<")
+    tap=int(input())
     #C=raw_input()
     #p,q,r=8,0,0
     if(tap==1):
@@ -24,29 +24,29 @@ def bucket(n):
     elif(tap==3):
         p,q,r=0,0,3
     else:
-        print "First fill  the Bucket to play!!!!" 
-    print "A = ",p,"B = ",q,"C = ",r
+        print("First fill  the Bucket to play!!!!") 
+    print("A = ",p,"B = ",q,"C = ",r)
     while(n>0 and (tap==1 or tap==2 or tap==3)):
-        a,b=(raw_input().split())
-        if(a=='A' and b=='B' and p<>0 and q<>5):
+        a,b=(input().split())
+        if(a=='A' and b=='B' and p!=0 and q!=5):
             e=5-q
             p,q=p-e,q+e
             sa=display(p)
             sb=display(q)
             sc=display(r)
             for i in range(len(sa)):
-                print sa[i]+'     '+ sb[i]+'     '+ sc[i]
-            print "    ",p,"            ",q,"             ",r
-        elif(a=='A' and b=='C' and p<>0 and r<>3):
+                print(sa[i]+'     '+ sb[i]+'     '+ sc[i])
+            print("    ",p,"            ",q,"             ",r)
+        elif(a=='A' and b=='C' and p!=0 and r!=3):
             d=3-r
             p,r=p-d,r+d
             sa=display(p)
             sb=display(q)
             sc=display(r)
             for i in range(len(sa)):
-                print sa[i]+'     '+ sb[i]+'     '+ sc[i]
-            print "    ",p,"            ",q,"             ",r
-        elif(a=='B' and b=='C' and q<>0 and r<>3):
+                print(sa[i]+'     '+ sb[i]+'     '+ sc[i])
+            print("    ",p,"            ",q,"             ",r)
+        elif(a=='B' and b=='C' and q!=0 and r!=3):
             c=3-r
             if(c<q):
                  q,r=q-c,r+c
@@ -56,27 +56,27 @@ def bucket(n):
             sb=display(q)
             sc=display(r)
             for i in range(len(sa)):
-                print sa[i]+'     '+ sb[i]+'     '+ sc[i]
-            print "    ",p,"            ",q,"             ",r
-        elif(a=='B' and b=='A' and q<>0):
+                print(sa[i]+'     '+ sb[i]+'     '+ sc[i])
+            print("    ",p,"            ",q,"             ",r)
+        elif(a=='B' and b=='A' and q!=0):
             p=p+q
             q=0
             sa=display(p)
             sb=display(q)
             sc=display(r)
             for i in range(len(sa)):
-                print sa[i]+'     '+ sb[i]+'     '+ sc[i]
-            print "    ",p,"            ",q,"             ",r
-        elif(a=='C' and b=='A' and r<>0):
+                print(sa[i]+'     '+ sb[i]+'     '+ sc[i])
+            print("    ",p,"            ",q,"             ",r)
+        elif(a=='C' and b=='A' and r!=0):
             p=p+r
             r=0
             sa=display(p)
             sb=display(q)
             sc=display(r)
             for i in range(len(sa)):
-                print sa[i]+'     '+ sb[i]+'     '+ sc[i]
-            print "    ",p,"            ",q,"             ",r
-        elif(a=='C' and b=='B' and r<>0 and q<>5):
+                print(sa[i]+'     '+ sb[i]+'     '+ sc[i])
+            print("    ",p,"            ",q,"             ",r)
+        elif(a=='C' and b=='B' and r!=0 and q!=5):
             g=5-q
             if(g<r):
                 r,q=r-g,q+g
@@ -86,10 +86,10 @@ def bucket(n):
             sb=display(q)
             sc=display(r)
             for i in range(len(sa)):
-                print sa[i]+'     '+ sb[i]+'     '+ sc[i]
-            print "    ",p,"            ",q,"             ",r
+                print(sa[i]+'     '+ sb[i]+'     '+ sc[i])
+            print("    ",p,"            ",q,"             ",r)
         else:
-            print "Wrong Move!!!"
+            print("Wrong Move!!!")
         n=n-1
     if(p==4 and q==4):
         if(n==7):
@@ -100,19 +100,19 @@ def bucket(n):
         return 0
 
 ch='Y'
-while(ch<>'N'):
-    print "-------------------------------------------------------------------------------------------------------------"
-    print "Rules: You are allowed to enter either 'A' or 'B' or 'C' \nJust enter your moves : From which the bucket is emptied to the bucket getting filled.."
-    print "Let's start the game..."
-    print "-------------------------------------------------------------------------------------------------------------"
-    print "A B C are buckets of 8ltr 5ltr and 3ltr \nMake 4ltrs in any one bucket"
+while(ch!='N'):
+  
+    print("Rules: You are allowed to enter either 'A' or 'B' or 'C' \nJust enter your moves : From which the bucket is emptied to the bucket getting filled..")
+    print("Let's start the game...")
+    print("-------------------------------------------------------------------------------------------------------------")
+    print("A B C are buckets of 8ltr 5ltr and 3ltr \nMake 4ltrs in any one bucket")
     buck=bucket(14)
     if(buck==1):
-        print "You Won in minimum steps   Congo!!!!!!!"
+        print("You Won in minimum steps   Congo!!!!!!!")
     elif(buck==2):
-        print "You won ....but try to do in minimum steps....better luck next time!!!"
+        print("You won ....but try to do in minimum steps....better luck next time!!!")
     else:
-        print "You lose !!!!!!!"
-    print "Do You wish to play another game? Press Y or N only"
-    ch=raw_input()
+        print("You lose !!!!!!!")
+    print("Do You wish to play another game? Press Y or N only")
+    ch=input()
     
